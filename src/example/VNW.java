e0b189f5618dca2f85dea3e4667e86737bde9c30
Misaka <misakachan2545@gmail.com>
2021-05-11 15:15:38 +0300

    Nya

--- src/example/VNW.java
+++ src/example/VNW.java
@@ -14,7 +14,7 @@ import mindustry.world.blocks.storage.*;
 import java.util.HashMap;
 import java.util.Iterator;
 
-public class VNW extends Plugin{
+public class VNW extends Plugin {
 
     HashMap<String, Boolean> isVoted = new HashMap<>();
     int votes = 0;
@@ -22,39 +22,69 @@ public class VNW extends Plugin{
     int limit;
     int voteTime = 600;
     int updateInterval = 5;
+
     @Override
-    public void init(){
+    public void init() {
         Events.on(EventType.PlayerJoin.class, t -> {
             isVoted.put(t.player.uuid(), false);
         });
         Events.on(EventType.PlayerLeave.class, r -> {
-            if(isVoted.get(r.player.uuid())) {
+            if (isVoted.get(r.player.uuid())) {
                 isVoted.remove(r.player.uuid());
                 votes--;
             }
+        });
+        Vars.netServer.admins.addChatFilter((player1, text) -> {
+            if (!isVotingStarted) return text;
+            if (text.equals("y")) {
+                if (isVoted.get(player1.uuid())) {
+                    player1.sendMessage("[scarlet]Ты уже проголосовал!");
+                    return null;
+                }
+                isVoted.put(player1.uuid(), true);
+                votes++;
+                Call.sendMessage(player1.name() + " [accent]проголосовал(а) [green]за[] скип волны.[cyan]" + votes + "/" + limit);
+                if (votes == limit) {
+                    Call.sendMessage("[green]Голосование закончилось успешно! Пропускаем волну...");
+                    Vars.logic.runWave();
+                    votes = 0;
+                    voteTime = 5;
+                    updateInterval = 0;
+                    Groups.player.forEach(p -> isVoted.put(p.uuid(), false));
+                }
+                return null;
+            } else if (text.equals("n")) {
+                if (isVoted.get(player1.uuid())) {
+                    player1.sendMessage("[scarlet]Ты уже проголосовал!");
+                    return null;
+                }
+                isVoted.put(player1.uuid(), true);
+                votes--;
+                Call.sendMessage(player1.name() + " [accent]проголосовал(а) [scarlet]против[]скипа волны.[cyan]" + votes + "/" + limit);
+                return null;
+            }
+            return text;
         });
     }
 
 
     @Override
-    public void registerServerCommands(CommandHandler handler){
+    public void registerServerCommands(CommandHandler handler) {
     }
 
 
     @Override
-    public void registerClientCommands(CommandHandler handler){
+    public void registerClientCommands(CommandHandler handler) {
         handler.<Player>register("newwave", "Голосование за скип волны", (c, player) -> {
-            if(isVotingStarted) {
+            if (isVotingStarted) {
                 player.sendMessage("[scarlet]Голосование уже идёт!");
                 return;
             }
-            if(Groups.player.size() > 4) {
+            if (Groups.player.size() > 4) {
                 limit = 3;
-            }
-            else if(Groups.player.size() < 4) {
+            } else if (Groups.player.size() < 4) {
                 limit = 2;
-            }
-            else if(Groups.player.size() == 1) {
+            } else if (Groups.player.size() == 1) {
                 limit = 1;
             }
             votes++;
@@ -64,53 +94,17 @@ public class VNW extends Plugin{
             isVoted.put(player.uuid(), true);
             Timer.schedule(() -> {
                 voteTime -= updateInterval;
-                if(voteTime < 1){
+                if (voteTime < 1) {
                     Call.sendMessage("[scarlet]Голосование провалилось.");
                     voteTime = 5;
                     updateInterval = 0;
                     votes = 0;
-                    Iterator it = Groups.player.iterator();
-                    while (it.hasNext()) {
-                        Player player2 = (Player)it.next();
-                        isVoted.put(player2.uuid(), false);
-                    }
+                    Groups.player.forEach(p -> {
+                        isVoted.put(p.uuid(), false);
+                    });
+                    isVotingStarted=false;
                 }
             }, 0, updateInterval);
-            Vars.netServer.admins.addChatFilter((player1, text) -> {
-                if(text.equals("y")) {
-                    if(isVoted.get(player1.uuid())) {
-                        player1.sendMessage("[scarlet]Ты уже проголосовал!");
-                        return null;
-                    }
-                    isVoted.put(player1.uuid(), true);
-                    votes++;
-                    Call.sendMessage(player1.name() + " [accent]проголосовал(а) [green]за[]скип волны.[cyan]" + votes + "/" + limit);
-                    if(votes == limit) {
-                        Call.sendMessage("[green]Голосование закончилось успешно! Пропускаем волну...");
-                        Vars.logic.runWave();
-                        votes = 0;
-                        voteTime = 5;
-                        updateInterval = 0;
-                        Iterator it = Groups.player.iterator();
-                        while (it.hasNext()) {
-                            Player player2 = (Player)it.next();
-                            isVoted.put(player2.uuid(), false);
-                        }
-                    }
-                    return null;
-                }
-                else if(text.equals("n")) {
-                    if(isVoted.get(player1.uuid())) {
-                        player1.sendMessage("[scarlet]Ты уже проголосовал!");
-                        return null;
-                    }
-                    isVoted.put(player1.uuid(), true);
-                    votes--;
-                    Call.sendMessage(player1.name() + " [accent]проголосовал(а) [scarlet]против[]скипа волны.[cyan]" + votes + "/" + limit);
-                    return null;
-                }
-                return text;
-            });
         });
     }
 }
